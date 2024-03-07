f7778b46a232e997e6bab53b04853cff03c436ef
vatsal2004 <153715547+vatsal633@users.noreply.github.com>
2024-03-07 15:57:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function change_color() {
     /// ........ color change of header.........
-    document.getElementsByClassName("navbar")[0].style.backgroundColor = 'white';
+    document.getElementsByClassName("navbar")[0].style.backgroundColor = 'rgb(217 238 225)';
     //document.getElementById('navbar').style.backgroundColor = 'white';
 
     
@@ -17,7 +17,7 @@ function change_color() {
     document.getElementById('li2').style.fontWeight = 'normal'
     document.getElementById('li3').style.fontWeight = 'normal'
     document.getElementById('theme').style.display = 'none'
-    document.getElementById('logo-txt').style.color = 'black'
+    document.getElementById('txt').style.color = 'black'
 
     //..... code for change color of body...........
     // body color
@@ -26,8 +26,28 @@ function change_color() {
         skill_section[i].style.backgroundColor = 'white'
     }
     
+    document.getElementsByClassName('skill_section')[0].style.backgroundColor = '#d9eee1'
     document.getElementById('moon').style.cursor = 'pointer'
+
+    //change the colors of cards
+    let cards = document.getElementsByClassName('github')
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].style.backgroundColor = '#e7e9eb'
+        cards[i].style.color = 'black'
+    }
+    document.getElementsByClassName('h2')[0].firstElementChild.style.color = 'black'
+    document.getElementsByClassName('bottom')[0].style.borderColor = 'black'
+    let btn = document.getElementById('btn')
+    btn.style.backgroundColor = "#282a35"
+    btn.style.color = 'white'
+
+
+    //changing bg image 
+
+    let img = document.getElementsByClassName('bgimg')[0].style.backgroundImage ="URL('https://img.freepik.com/free-photo/abstract-blur-empty-green-gradient-studio-well-use-as-backgroundwebsite-templateframebusiness-report_1258-102220.jpg?size=626&ext=jpg&ga=GA1.1.1395880969.1709769600&semt=ais')"
+    
 }
+
 
 function dark() {
     document.getElementById('navbar').style.backgroundColor = "#222438";
@@ -40,11 +60,24 @@ function dark() {
     document.getElementById('li2').style.fontWeight = 'normal'
     document.getElementById('li3').style.fontWeight = 'normal'
     document.getElementById('theme').style.display = 'block'
-    document.getElementById('logo-txt').style.color = 'white'
-    let skill_section = document.getElementsByClassName('skill_section')
-    for (let i = 0; i < skill_section.length; i++) {
-        skill_section[i].style.backgroundColor = '#222438'
+    document.getElementById('txt').style.color = 'white'
+
+    document.getElementsByClassName('skill_section')[0].style.backgroundColor = '#222438'
+
+    let cards = document.getElementsByClassName('github')
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].style.backgroundColor = '#222438'
+        cards[i].style.color = 'white'
     }
+    document.getElementsByClassName('h2')[0].firstElementChild.style.color = '#1eff97'
+    document.getElementsByClassName('bottom')[0].style.borderColor = '#1eff97'
+
+    let btn = document.getElementById('btn')
+    btn.style.backgroundColor = "#f8e9a1"
+    btn.style.color = 'black'
+
+    let img = document.getElementsByClassName('bgimg')[0].style.backgroundImage ="URL('https://i.pinimg.com/736x/6c/bb/36/6cbb36a34afe45944dc1280aed5f9c0c.jpg')"
+    
 }
 
 // function hover(){
@@ -54,4 +87,3 @@ function dark() {
 //     document.getElementById('li3').style.backgroundColor =  'red'
 
 // }
-
